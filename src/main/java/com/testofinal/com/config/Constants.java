c35a8276dfbc683bdf88f04f006b3efcc0048f45
1JEBO1 <josue.briceno@zabud.com.co>
2021-01-25 17:16:51 -0500

    Initial version of testeofinal generated by JHipster-6.10.5

---
+++ src/main/java/com/testofinal/com/config/Constants.java
@@ -0,0 +1,12 @@
+package com.testofinal.com.config;
+
+/**
+ * Application constants.
+ */
+public final class Constants {
+
+    public static final String SYSTEM_ACCOUNT = "system";
+
+    private Constants() {
+    }
+}
---
+++ src/main/java/com/testofinal/com/config/audit/package-info.java
@@ -0,0 +1,4 @@
+/**
+ * Audit specific code.
+ */
+package com.testofinal.com.config.audit;
